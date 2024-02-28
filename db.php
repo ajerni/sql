ac4d3a32863dac88607092ddc388918e02dd00e7
„Andi <ajerni@gmail.com>
2024-02-28 16:30:24 +0100

    delete added

--- db.php
+++ db.php
@@ -23,6 +23,21 @@ class DB {
     $stmt->execute(['name' => $name, 'email' => $email]);
     echo "data inserted";
   }
+
+  public function getData() {
+    $sql = "SELECT * FROM userdetails";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC); //retuns data as Associative Array
+    return $data;
+  }
+
+  public function deleteData($id){
+    $sql = "DELETE FROM userdetails WHERE id = :id";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute(['id' => $id]);
+    echo "ID: " . $id . " was deleted";
+  }
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,12 @@ require_once('db.php');
 
 $db = new DB();
 
+// Delete Data
+if(isset($_POST['deleteData'])){
+$id = $_POST['id'];
+$db->deleteData($id);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,13 +17,35 @@ $db = new DB();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DocumentUser Details</title>
+    <title>User Details</title>
+    <style>
+        body{
+            font-family: "Robto";
+        }
+    </style>
 </head>
 <body>
+    <h1>Insert</h1>
     <form action="insert.php" method="POST">
         <input type="text" placeholder="Name" name="name">
         <input type="text" placeholder="Email" name="email">
         <input type="submit" value="Insert" name="insertData">
     </form>
+
+    <h1>Delete</h1>
+    <form method="POST">
+        <input type="text" placeholder="Id" name="id">
+        <input type="submit" value="Delete" name="deleteData">
+    </form>
+
+    <h1>Data</h1>
+    <?php
+    $data = $db->getData();
+    foreach($data as $i){
+        echo $i['name'] . " - " . $i['email'] . "<br>";
+    }
+
+    ?>
+
 </body>
 </html>
